db25dfa4cfc596ea67744e160ad90f63f0eb9689
Mfrancisco <morelfrancisco18@hotmail.com>
2024-11-21 20:55:22 -0300

    creado archivo de arreglos de rtos

---
+++ Tpi-diseño/patentes.js
@@ -0,0 +1,9 @@
+
+let rtos = [
+        { patente: 'ABC123', marca: 'Toyota', modelo: 'Corolla', año: '2019' },
+        { patente: 'XYZ789', marca: 'Ford', modelo: 'F-150', año: '2022' },
+        { patente: 'LMN456', marca: 'Honda', modelo: 'Civic', año: '2021' },
+        { patente: 'DEF987', marca: 'Chevrolet', modelo: 'Malibu', año: '2018' }
+    ];
+
+    export default rtos;
